54e4cf3744a88abe5a50141882c0a30b3296f043
Anurag Kulkarni <kanurag0801@gmail.com>
2022-05-19 22:39:06 +0530

    Initializing Spring Boot Project

---
+++ src/test/java/com/anurag/springboot/firstspringbootapp/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.springboot.firstspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
